3feeb77d78927c13f6a257b56b61f717274be584
swapnilborate78 <swapnilborate78@gmail.com>
2023-05-04 17:44:22 +0530

    admin

---
+++ Front End/HotelManagementSystem/src/app/add-booking/add-booking.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Booking } from '../booking';
+import { Customer } from '../customer';
+import { BookingService } from '../Service/booking.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CustomerService } from '../Service/customer.service';
+
+@Component({
+  selector: 'app-add-booking',
+  templateUrl: './add-booking.component.html',
+  styleUrls: ['./add-booking.component.css']
+})
+export class AddBookingComponent implements OnInit {
+  id:number;
+  result:number;
+  customer:Customer = new Customer(0,"","","","","","");
+  booking :Booking = new Booking(0,0,0,'',0);
+  constructor(private bookingService:BookingService,private route:ActivatedRoute,private router:Router,private customerService:CustomerService){}
+ 
+  ngOnInit(): void {
+    this.id= this.route.snapshot.params['id'];
+    this.customerService.getCustomerById(this.id).subscribe(data=>{this.customer=data;},error=>console.log(error));
+   }
+ 
+    
+
+    Calculate(v:string,b:string){
+      this.result=parseInt(v)*parseInt(b);
+      
+  
+     }
+     
+    Book(){
+      console.log(this.result);
+      console.log(this.customer.employeeId);
+      this.booking.totalAmount=this.result;
+      console.log(this.booking);
+      this.bookingService.bookRoom(this.booking).subscribe(data=>
+        this.router.navigateByUrl("/dashboard/bookings"));
+  
+      }
+
+
+}
